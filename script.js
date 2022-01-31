5ae2e6e9f77e856180665c2a6fdaa2821e76b90e
William Araujo <williamf1f2@gmail.com>
2022-01-31 19:22:55 -0300

    Adiciona responsividade ao layout para dispositivos móveis

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const backToTopButton = document.querySelector('.scroll-to-top-button');
+const whatsappHref = document.querySelector('#whatsapp-send-message');
 
 // trick to remove the url hash
 function removeHashFromUrl() {
   history.replaceState({}, document.title, window.location.href.split('#')[0]);
 }
+
+function addWhasAppHref() {
+  console.log('whatsappHref', whatsappHref)
+  const messageToSend = 'Olá, vim pelo site https://limadesentupidora.com.br. Preciso de mais informações sobre!';
+  whatsappHref.href = `https://api.whatsapp.com/send?phone=5513996255448&text=${encodeURI(messageToSend)}`;
+}
+
 
 // on button click its returns to the top of page
 backToTopButton.addEventListener('click', () => {
@@ -17,4 +25,5 @@ backToTopButton.addEventListener('click', () => {
 
 window.onload = () => {
   removeHashFromUrl();
+  addWhasAppHref();
 }
