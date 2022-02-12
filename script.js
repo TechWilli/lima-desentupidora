45beacf320d146fb33c269879374f6bda6bbbcd0
William Araujo <williamf1f2@gmail.com>
2022-02-12 19:04:14 -0300

    Faz ajustes de responsividade e correção de estilos gerais do site

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const backToTopButton = document.querySelector('.scroll-to-top-button');
-const whatsappHref = document.querySelector('#whatsapp-send-message');
+const whatsappHref = document.querySelectorAll('.whatsapp-send-message');
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const navMenu = document.querySelector('.nav-items-wrapper')
 
 // trick to remove the url hash
 function removeHashFromUrl() {
@@ -7,21 +9,33 @@ function removeHashFromUrl() {
 }
 
 function addWhatsAppHref() {
-  console.log('whatsappHref', whatsappHref)
+  // console.log('whatsappHref', whatsappHref)
   const messageToSend = 'Olá, vim pelo site https://limadesentupidora.com.br. Preciso de mais informações sobre!';
-  whatsappHref.href = `https://api.whatsapp.com/send?phone=5513996255448&text=${encodeURI(messageToSend)}`;
+  whatsappHref[0].href = `https://api.whatsapp.com/send?phone=5513996255448&text=${encodeURI(messageToSend)}`;
+  whatsappHref[1].href = `https://api.whatsapp.com/send?phone=5513996255448&text=${encodeURI(messageToSend)}`;
 }
 
+function handleWhatsappClick() {
+  whatsappHref[1].click()
+}
 
-// on button click its returns to the top of page
-backToTopButton.addEventListener('click', () => {
+function goToTop() {
   window.scrollTo({
     top: 0,
     behavior: "smooth"
   });
 
   removeHashFromUrl();
-});
+}
+
+// on button click its returns to the top of page
+backToTopButton.addEventListener('click', goToTop);
+
+hamburgerMenu.addEventListener('click', () => {
+  // alert('clicou no hamburger')
+  console.log('clicou')
+  navMenu.classList.toggle('hide-menu')
+})
 
 window.onload = () => {
   removeHashFromUrl();
